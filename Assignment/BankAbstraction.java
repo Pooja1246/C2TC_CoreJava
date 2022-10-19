f56d206ba8181790493d9fe57df34e734b47602c
Pooja1246 <113159592+Pooja1246@users.noreply.github.com>
2022-10-19 16:03:13 +0530

    Delete BankAbstraction.java

--- Assignment/BankAbstraction.java
+++
@@ -1,8 +0,0 @@
-public class BankAbstraction {
-    public static void main(String[] args) {
-        centralBank cb = new centralBank("Maya", 765940, "Savings", "Hosur");
-        cb.withdraw();
-        cb.deposit();
-        cb.getinterest();
-    }
-}
